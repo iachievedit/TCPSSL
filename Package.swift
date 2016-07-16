1930bcb49212fa0a9ab01ede39f4b839ce14051b
Joseph Bell <joe@iachieved.it>
2016-07-16 16:16:36 -0500

    Add unit test for receive upTo

--- Package.swift
+++ Package.swift
@@ -4,6 +4,6 @@ let package = Package(
     name: "TCPSSL",
     dependencies: [
         .Package(url: "https://github.com/VeniceX/TCP.git", majorVersion: 0, minor: 8),
-        .Package(url: "https://github.com/Zewo/OpenSSL.git", majorVersion: 0, minor: 8),
+        .Package(url: "https://github.com/iachievedit/OpenSSL", majorVersion: 0, minor:9),
     ]
 )
--- Tests/TCPSSL/TCPSSLTests.swift
+++ Tests/TCPSSL/TCPSSLTests.swift
@@ -2,6 +2,20 @@ import XCTest
 @testable import TCPSSL
 
 class TCPSSLTests: XCTestCase {
+    func testReceive() {
+        do {
+        let server = "api.github.com"
+        let httpRequest:Data = "GET /zewo/repos HTTP/1.1\r\nHost: api.github.com\r\nUser-Agent: venicex/0.8.0\r\nAccept: */*\r\n\r\n"
+        let secureConn = try TCPSSLConnection(host:server, port:443)
+        try secureConn.open()
+        try secureConn.send(httpRequest, timingOut:.never)
+        let httpData = try secureConn.receive(upTo:4, timingOut:.never)
+        XCTAssert(httpData.count == 4,  "Expected 4 bytes, received \(httpData.count)")
+        } catch {
+            print(error)
+        }
+    }
+
     func testReality() {
         XCTAssert(2 + 2 == 4, "Something is severely wrong here.")
     }
@@ -10,7 +24,8 @@ class TCPSSLTests: XCTestCase {
 extension TCPSSLTests {
     static var allTests : [(String, (TCPSSLTests) -> () throws -> Void)] {
         return [
-           ("testReality", testReality),
+            ("testReceive", testReceive),
+            ("testReality", testReality),
         ]
     }
 }
